b17e657d8cacd60d6736ff268acce5ff2fb376d0
Eron Alves <eron.a.silva@capgemini.com>
2022-11-07 14:41:39 -0300

    Make header general structure

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h2>$$$ Money Control App $$$</h2>
+      </header>
+      <main>
+        <header>
+          <h3>Movimentações</h3>
+          <nav>
+            <section class="years">
+              <a href="#">2020</a> <a href="#">2021</a> <a href="#">2022</a>
+            </section>
+            <section class="months">
+              <a href="#">JAN</a> <a href="#">FEV</a> <a href="#">MAR</a>{" "}
+              <a href="#">ABR</a> <a href="#">MAI</a> <a href="#">JUN</a>{" "}
+              <a href="#">JUL</a> <a href="#">AGO</a> <a href="#">SET</a>{" "}
+              <a href="#">OUT</a> <a href="#">NOV</a> <a href="#">DEZ</a>
+            </section>
+          </nav>
+        </header>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
